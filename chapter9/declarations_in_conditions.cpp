5d44e7787e1b7e9c5cb023c16248b49d4a0ff30e
Eddyding <920398694@qq.com>
2016-10-14 14:58:51 +0800

    statements

--- chapter9/declarations_in_conditions.cpp
+++ chapter9/declarations_in_conditions.cpp
@@ -19,12 +19,41 @@ if(double d = prim(true)){
 	break;
 }
 
+
+
 The scope of d extends from its point of declaration to the end of the statement
 
 that the condition controls.For example,Had there been an else-branch to the if-statement,
 d would be in scope on both branches.
 
+
+
+The  obvious and traditional alternatives is to declare d before the condition,
+However,this opens the scope(literally) for the use of d before its initialization 
+or after its intended useful life:
+
+double d;
+//...
+d2 = d; // oops!
+
+//...
+if(double d = prim(true)){
+	left /= d;
+	break;
+}
+//...
+
+
+d = 2.0f;// two unrelated uses of d
+
+
+
+
 如下：
+
+
+
+
 */
 
 int 
@@ -36,5 +65,21 @@ main()
 		{
 		cout<<a<<endl;
 	}
+
+	int a = 12;
+    if(int b = a){
+		cout<<b<<endl;
+	}else
+		{
+		cout<<b<<endl;
+	}
 	return 0;
 }
+
+/*
+In addition to the logical benefits of declaring variables in conditions,doing 
+so also yields the most compact source code.
+
+A declaration in a condition must declare and initialize a single variable or const.
+
+*/
---
+++ chapter9/statements.cpp
@@ -0,0 +1,22 @@
+// a  programmer is a machine for turning caffeine into code.
+
+note that a declaration is a statement and that an expression 
+becomes a statement when you add a semicolon at its end.
+
+Unlike an expression, a statement does not have a value.
+
+Instead,statements are used to specify the order of execution.
+
+a = b+c;// expression statement
+if(a == 7)// if-statement
+	b =  9;//execute if and only if a == 7
+
+Logically, a = b+c is executed before the if,as everyone would expect.
+
+A compiler may reorder code to improve performance as long as the 
+result is identical to that of the simple order of execution.
+
+
+Statement Summary:
+
+
